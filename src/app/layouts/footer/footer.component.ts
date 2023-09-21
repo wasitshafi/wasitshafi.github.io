d944ef25459a8efd5c2288710031368bd91c4301
wasitshafi <wasitshafi700@gmail.com>
2023-09-22 03:33:06 +0530

    switched to @forward/@use from @import for css style

--- src/app/layouts/footer/footer.component.ts
+++ src/app/layouts/footer/footer.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
 
+import { Constants } from '@common/constants';
+
 @Component({
 	selector: 'app-footer',
 	templateUrl: './footer.component.html',
 	styleUrls: ['./footer.component.scss'],
 })
-export class FooterComponent {}
+export class FooterComponent {
+	constructor(public constants: Constants) {}
+}

b010f1543c40c73d831dbe7edf67e332585182b8
wasitshafi <wasitshafi700@gmail.com>
2023-10-03 02:28:41 +0530

    partial updated about  section

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
 
+import { Constants } from '@common';
+
 @Component({
 	selector: 'app-about',
 	templateUrl: './about.component.html',
 	styleUrls: ['./about.component.scss'],
 })
-export class AboutComponent {}
+export class AboutComponent {
+	constructor(public constants: Constants) {}
+}

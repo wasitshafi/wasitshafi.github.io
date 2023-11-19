0abfd13e36284415f028f904a56213da5e437daf
wasitshafi <wasitshafi700@gmail.com>
2023-11-20 03:58:43 +0530

    added scroll to top feature

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 import { Constants } from '@shared';
 import { UserProfileService, AppDataService } from '@services';
@@ -12,10 +12,42 @@ import { UserProfileService, AppDataService } from '@services';
 		'./home-responsive.component.scss',
 	],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit, OnDestroy {
+	public SCROLL_TO_TOP_THRESHOLD = 600;
+
 	public constructor(
 		public appDataService: AppDataService,
 		public constants: Constants,
 		public userProfileService: UserProfileService
 	) {}
+
+	ngOnInit(): void {
+		document.addEventListener('scroll', this.handleWindowScroll.bind(this));
+	}
+
+	ngOnDestroy(): void {
+		document.removeEventListener('scroll', this.handleWindowScroll);
+	}
+
+	public handleWindowScroll() {
+		const backToTopButton = document.getElementById(
+			this.constants.STYLE_PREFIX + 'scroll-to-top-button'
+		);
+
+		//  for more info refer: https://stackoverflow.com/questions/19618545/body-scrolltop-vs-documentelement-scrolltop-vs-window-pageyoffset-vs-window-scro
+		if (
+			document.body.scrollTop > this.SCROLL_TO_TOP_THRESHOLD ||
+			document.documentElement.scrollTop > this.SCROLL_TO_TOP_THRESHOLD
+		) {
+			backToTopButton?.style?.setProperty('visibility', 'visible');
+			backToTopButton?.style?.setProperty('opacity', '1');
+		} else {
+			backToTopButton?.style?.setProperty('visibility', 'hidden');
+			backToTopButton?.style?.setProperty('opacity', '0');
+		}
+	}
+
+	public handleScrollToTop() {
+		window.scrollTo(0, 0);
+	}
 }
